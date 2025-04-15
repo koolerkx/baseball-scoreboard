ad785e96ab5423f8e16180717334c7393fd0fd18
Kooler Fan <koolerkx@gmail.com>
2025-04-15 14:18:58 +0900

    temp: ball count

--- scoreboard.h
+++ scoreboard.h
@@ -1,12 +1,21 @@
 #pragma once
 #include "team.h"
 
+
+
 class ScoreBoard {
     ScoreBoard();
     static ScoreBoard* scoreboard_;
 public:
     void print();
     static ScoreBoard* getInstance();
+
+    struct {
+        int strike = 0;
+        int ball = 0;
+        int out = 0;
+    };
+
 private:
     Team team_home = Team{ "home" };
     Team team_away = Team{ "away" };
